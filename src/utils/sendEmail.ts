79be8b0a99330584f167072fbf22f1966ea0bd30
DecimDev <devburner505@gmail.com>
2024-09-13 10:07:11 -0400

    fixing page links

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,15 +1,22 @@
 import sgMail from '@sendgrid/mail';
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
-const sendEmail = async (name: string, email: string, message: string) => {
+const sendEmail = async (to: string, subject: string, text: string) => {
     const msg = {
-        to: 'jacob.cadiente@gmail.com',
-        from: 'jacob.cadiente@gmail.com',
-        subject: `Contact Form Submission from ${name}`,
-        text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
+        to,
+        from: 'jacob.cadiente@gmail.com', // Use your verified sender
+        subject,
+        text,
     };
-    await sgMail.send(msg);
+
+    try {
+        await sgMail.send(msg);
+        console.log('Email sent');
+    } catch (error) {
+        console.error('Error sending email:', error);
+        throw error;
+    }
 };
 
 export default sendEmail;
